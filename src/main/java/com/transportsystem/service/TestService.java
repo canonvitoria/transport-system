8057b004ff3d949c08419ebe00a02de8ef3d1f01
VitoriaCanon <vitoriacanon1212@gmail.com>
2026-01-30 21:33:44 -0300

    refactor: remove unused test configuration, controller, and service classes

--- src/main/java/com/transportsystem/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.transportsystem.service;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TestService {
-   public String test(String name) {
-       return "Hello world from Service!" + name;
-   }
-}
